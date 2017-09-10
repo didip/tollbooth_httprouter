879b5139ef937d23b23b90e356ca1d6379e10352
Didip Kerabat <didipk@gmail.com>
2017-09-09 21:11:42 -0700

    Update to conform with v2.x.x API.

--- tollbooth_httprouter.go
+++ tollbooth_httprouter.go
@@ -4,16 +4,18 @@ import (
 	"net/http"
 
 	"github.com/didip/tollbooth"
-	"github.com/didip/tollbooth/config"
+	"github.com/didip/tollbooth/limiter"
 	"github.com/julienschmidt/httprouter"
 )
 
 // RateLimit is a rate limiting middleware
-func LimitHandler(handler httprouter.Handle, limiter *config.Limiter) httprouter.Handle {
+func LimitHandler(handler httprouter.Handle, lmt *limiter.Limiter) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		httpError := tollbooth.LimitByRequest(limiter, r)
+		httpError := tollbooth.LimitByRequest(lmt, r)
 		if httpError != nil {
-			http.Error(w, httpError.Message, httpError.StatusCode)
+			w.Header().Add("Content-Type", lmt.GetMessageContentType())
+			w.WriteHeader(httpError.StatusCode)
+			w.Write([]byte(httpError.Message))
 			return
 		}
 
